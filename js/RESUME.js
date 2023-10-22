f6d923e65fb592665484b35a8a1cad536abd968d
stamp <stampzdc@gmail.com>
2023-10-22 17:01:32 +0700

    first commit

--- js/RESUME.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('#red').onclick = function(){
-        document.querySelector('strong').style.color = 'red';   
-     };
-     document.querySelector('#green').onclick = function(){
-        document.querySelector('strong').style.color = 'green';
-         
-    };
-});
